d8b97c433a12bb6cc2aee90dada3a5852dd731dd
percevil <peeevil@gmail.com>
2020-10-12 18:09:44 -0700

    added more animation /added ui pages

--- assets/js/animate.js
+++ assets/js/animate.js
@@ -1,25 +1,76 @@
 			
-// gsap.set('.ninja2', { transformOrigin: '50% 50%' });
 
-const tl = gsap.timeline({ repeat: -1 });
+const tl = gsap.timeline({ });
+
 
 tl.from('.chevron', {
-  duration: 1.5,
+  duration: 1,
   opacity: 0,
   scale: 1.3,
-  ease: 'back',
-  stagger: 0.5});
+  ease: 'SloMo',
+  stagger: .4,
+  repeat: -1,
+});
 
-// tl.from('.chevron2', {
-//     duration: 1.5,
-//     opacity: 0,
-//     scale: 1.3,
-//     ease: 'back',
-//   });
+tl.to('.submit', {
+    duration: 2,
+    opacity: 0.3,
+    ease: 'linear', 
+    repeat: -1,
+});
 
-//   tl.from('.chevron1', {
-//     duration: 1.5,
-//     opacity: 0,
-//     scale: 1.3,
-//     ease: 'back',
-//   });
+
+tl.from('.circle5', {
+  // duration: --duration,
+  opacity: 0,
+  y: -1100,
+  stagger: 0.25,
+  yoyo:true,
+  repeat:1,
+});
+tl.from('.circle4', {
+  // duration: --duration,
+  opacity: 0,
+  y: -1100,
+  stagger: 0.25,
+  yoyo:true,
+  repeat:1,
+});
+tl.from('.circle3', {
+  // duration: --duration,
+  opacity: 0,
+  y: -1100,
+  stagger: 0.25,
+  yoyo:true,
+  repeat:1,
+});
+tl.from('.circle2', {
+  duration: 0.5,
+  opacity: 0,
+  y: -1100,
+  stagger: 0.25,
+  yoyo:true,
+  repeat:1,
+});
+tl.from('.circle1', {
+  // duration: --duration,
+  opacity: 1,
+  y: -550,
+  yoyo:true,
+  repeat:1,
+});
+
+tl.from('.circle', {
+  '--bubbleColor': '#AD5AF2',
+  duration: 4,
+  yoyo: true,
+  repeat: -1,
+});
+
+
+tl.to('.circle', {
+  duration: 10,
+  rotationY: '180',
+  repeat: -1,
+});
+
